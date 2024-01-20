7ab18f0c1d585346a06e173e9d1c15782594f29d
Kyrylo Mikos <kiril.mik.os@gmail.com>
2024-01-20 10:00:41 +0200

    Add BRT-100-TRV quirk 2.

--- custom_components/better_thermostat/model_fixes/BRT-100-TRV.py
+++ custom_components/better_thermostat/model_fixes/BRT-100-TRV.py
@@ -1,10 +1,6 @@
 from homeassistant.components.climate.const import HVACAction
 
 def fix_local_calibration(self, entity_id, offset):
-    if self.attr_hvac_action == HVACAction.IDLE:
-        if offset < self.tolerance:
-            offset = self.tolerance
-    
     return offset
 
 
@@ -12,8 +8,8 @@ def fix_target_temperature_calibration(self, entity_id, temperature):
     _cur_trv_temp_s = self.real_trvs[entity_id]["current_temperature"]
 
     if self.attr_hvac_action == HVACAction.IDLE:
-        if temperature - _cur_trv_temp_s > 0.0:
-            temperature -= self.tolerance
+        if temperature - _cur_trv_temp_s < 3.0:
+            temperature = _cur_trv_temp_s - 3.0
 
     return temperature
 

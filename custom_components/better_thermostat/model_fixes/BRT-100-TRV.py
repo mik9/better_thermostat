a69ffb2c7b6817e859a6f74196055e3df1d39e78
Kyrylo Mikos <kiril.mik.os@gmail.com>
2024-01-22 14:52:30 +0200

    Add BRT-100-TRV quirk 3.

--- custom_components/better_thermostat/model_fixes/BRT-100-TRV.py
+++ custom_components/better_thermostat/model_fixes/BRT-100-TRV.py
@@ -1,5 +1,8 @@
 from homeassistant.components.climate.const import HVACAction
 
+from custom_components.better_thermostat.utils.const import CalibrationMode
+from custom_components.better_thermostat.utils.helpers import heating_power_valve_position
+
 def fix_local_calibration(self, entity_id, offset):
     return offset
 
@@ -7,9 +10,31 @@ def fix_local_calibration(self, entity_id, offset):
 def fix_target_temperature_calibration(self, entity_id, temperature):
     _cur_trv_temp_s = self.real_trvs[entity_id]["current_temperature"]
 
+    _calibration_mode = self.real_trvs[entity_id]["advanced"].get(
+        "calibration_mode", CalibrationMode.DEFAULT
+    )
+
+    if _calibration_mode == CalibrationMode.AGGRESIVE_CALIBRATION:
+        if self.attr_hvac_action == HVACAction.HEATING:
+            temperature = _cur_trv_temp_s + 3.0
+
+    if _calibration_mode == CalibrationMode.HEATING_POWER_CALIBRATION:
+        if self.attr_hvac_action == HVACAction.HEATING:
+            valve_position = heating_power_valve_position(self, entity_id)
+
+            if valve_position <= 0.25:
+                temp_diff = 1
+            elif valve_position <= 0.5:
+                temp_diff = 2
+            elif valve_position <= 0.75:
+                temp_diff = 3
+            else:
+                temp_diff = 4
+
+            temperature = _cur_trv_temp_s + temp_diff
+
     if self.attr_hvac_action == HVACAction.IDLE:
-        if temperature - _cur_trv_temp_s < 3.0:
-            temperature = _cur_trv_temp_s - 3.0
+        temperature = _cur_trv_temp_s - 3.0
 
     return temperature
 
